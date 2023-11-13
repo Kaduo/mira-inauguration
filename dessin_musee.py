04af62d22994d2e425256203248b1fc87fa7019d
Kaduo <balthazarpotet@gmail.com>
2023-11-13 11:27:24 +0100

    it works (:

--- dessin_musee.py
+++ dessin_musee.py
@@ -3,6 +3,7 @@ from utils import sort_edges
 import numpy as np
 from arms import get_big_drawing_arm, calibrate, draw_edges
 import skimage as ski
+from pebble import concurrent
 
 from photo2drawing import rgb2edges
 
@@ -12,15 +13,31 @@ above_origin = np.array([200, -120, 138])
 above_p1 = np.array([560, -120, 140])
 above_p2 = np.array([200, 138, 139])
 
-origin, p1, p2 = calibrate(arm, above_origin, above_p1, above_p2)
+@concurrent.process
+def make_converter(image):
+    origin, p1, p2 = calibrate(arm, above_origin, above_p1, above_p2)
+    converter = CoordinatesConverter(list(reversed(image.shape[:2])), origin, p1, p2)
+    return converter
 
-image = ski.io.imread("data/st jerome.jpg")
-converter = CoordinatesConverter(list(reversed(image.shape[:2])), origin, p1, p2)
 
-edges = rgb2edges(image, nb_edges=1000)
+@concurrent.process
+def process_image(image, nb_edges=1000):
 
-edges = sort_edges(edges)
+    print("I'm processing the IMAGE ")
 
-edges = converter.convert_list_of_points(edges)
+    image = ski.io.imread("data/st jerome.jpg")
+    edges = rgb2edges(image, nb_edges=nb_edges)
+    edges = sort_edges(edges)
+    return edges
 
-draw_edges(arm, edges, verbose=True)
+
+if __name__=="__main__":
+    image = ski.io.imread("data/st jerome.jpg")
+    future_edges = process_image(image)
+    future_converter = make_converter(image)
+
+    converter = future_converter.result()
+    edges = future_edges.result()
+    
+    edges = converter.convert_list_of_points(edges)
+    draw_edges(arm, edges, verbose=True)
