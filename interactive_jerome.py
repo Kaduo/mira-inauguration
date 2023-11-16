c2cc8105aae3b01b9e2508e7a36edb810ae3f83a
Kaduo <balthazarpotet@gmail.com>
2023-11-16 15:15:34 +0100

    hop

--- interactive_jerome.py
+++ interactive_jerome.py
@@ -20,7 +20,8 @@ def big_dawg(image, sigma, p, dilation_size=1):
     edge_image_otsu = 1 - (thresh_otsu ^ dilation(thresh_otsu, footprint=footprint))
     thresh_mean = dog_response > threshold_mean(dog_response)
     edge_image_mean = 1 - (thresh_mean ^ dilation(thresh_mean, footprint=footprint))
-    return dog_response, thresh_otsu, thresh_mean, edge_image_otsu, edge_image_mean
+    canny_edge_image = 1 - ski.feature.canny(ski.color.rgb2gray(image))
+    return dog_response, thresh_otsu, thresh_mean, edge_image_otsu, edge_image_mean, canny_edge_image
 
 
 init_sigma = 5
@@ -28,9 +29,9 @@ init_p = 5
 init_dilation_size = 3
 
 
-image = ski.io.imread("data/vinciguerra.jpeg")
+image = ski.io.imread("data/simoni.jpeg")
 
-dog_response, thresh_otsu, thresh_mean, edge_image_otsu, edge_image_mean = big_dawg(
+dog_response, thresh_otsu, thresh_mean, edge_image_otsu, edge_image_mean , canny_edge_image= big_dawg(
     image, sigma=init_sigma, p=init_p, dilation_size=init_dilation_size
 )
 
@@ -39,6 +40,7 @@ fig, ax = plt.subplots(2, 3)
 im_dog = ax[0, 0].imshow(dog_response, cmap="gray")
 im_thresh_otsu = ax[0, 1].imshow(thresh_otsu, cmap="gray")
 im_edge_otsu = ax[0, 2].imshow(edge_image_otsu, cmap="gray")
+ax[1,0].imshow(canny_edge_image, cmap="gray")
 im_thresh_mean = ax[1, 1].imshow(thresh_mean, cmap="gray")
 im_edge_mean = ax[1, 2].imshow(edge_image_mean, cmap="gray")
 
@@ -77,7 +79,7 @@ dilate_slider = Slider(
 
 # The function to be called anytime a slider's value changes
 def update(val):
-    dog_response, thresh_otsu, thresh_mean, edge_image_otsu, edge_image_mean = big_dawg(
+    dog_response, thresh_otsu, thresh_mean, edge_image_otsu, edge_image_mean, canny_edge_image = big_dawg(
         image, sigma=sigma_slider.val, p=p_slider.val, dilation_size=int(dilate_slider.val)
     )
     im_dog.set_data(dog_response)
