dce649ca2675f72c4ba16ee3ceffbeed2f97a718
nHuloux <nicolas@aflokkat.com>
2023-11-16 11:47:52 +0100

    ajout cadre

--- photomaton.py
+++ photomaton.py
@@ -29,6 +29,7 @@ import skimage as ski
 
 def process_frame(frame):
     res = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    res = cv2.flip(res,1)
     res = res[res.shape[0] // 2 :, :]
     return res
 
@@ -36,6 +37,30 @@ def get_frame(cap):
     _, frame = cap.read()
     return process_frame(frame)
 
+def show(image):
+    logo = image
+    # importing image on which we are going to  
+    # apply watermark 
+    img = cv2.imread("Data/cadre.png") 
+    h_logo, w_logo, _ = logo.shape 
+
+    # height and width of the image 
+    h_img, w_img, _ = img.shape 
+    center_y = int(h_img/2) 
+    center_x = int(w_img/2) 
+    
+    # calculating from top, bottom, right and left 
+    top_y = center_y - int(h_logo/2) -70
+    bottom_y = top_y + h_logo 
+    left_x = center_x - int(w_logo/2) 
+    right_x = left_x + w_logo 
+    destination = img[top_y:bottom_y, left_x:right_x] 
+    result = cv2.addWeighted(destination,0, logo, 1, 0) 
+    img[top_y:bottom_y, left_x:right_x] = result 
+    cv2.imshow("MIRA", img)
+    key = cv2.waitKey(1) & 0xFF
+    return key
+        
 def drawing_in_progress(edge_image):
     edge_image = np.array(edge_image, dtype=np.uint8) * 255
     edge_image = cv2.cvtColor(edge_image, cv2.COLOR_GRAY2BGR)
@@ -51,46 +76,35 @@ def drawing_in_progress(edge_image):
         2,
         cv2.LINE_AA,
     )
-
-    cv2.imshow("MIRA", im)
-    cv2.waitKey(1) & 0xFF
+    key = show(im)
 
 def photomaton_loop(cap, waiting_time=100):
     selected = False
     while not selected:
         frame = get_frame(cap)
-        im = cv2.putText(
-            frame,
-            "Portrait robot !",
-            (300, 100),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            2,
-            (0, 0, 0),
-            2,
-            cv2.LINE_AA,
-        )
-        cv2.imshow("MIRA", im)
-        key = cv2.waitKey(1) & 0xFF
+        
+        # height and width of the image 
+        key = show(frame)
+       
+        
         if key == ord("c"):
             for i in range(waiting_time, 0, -1):
                 im = cv2.putText(
                     frame,
-                    f"{round(i/30)}",
+                    f"{round(i/21)}",
                     (int(frame.shape[1] / 2), int(frame.shape[0] / 2)),
                     cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
                     5,
                     (0, 0, 0),
-                    2,
+                    5,
                     cv2.LINE_AA,
                 )
-                cv2.imshow("MIRA", im)
-                key = cv2.waitKey(1) & 0xFF
+                key = show(im)
                 time.sleep(0.01)
                 frame = get_frame(cap)
 
             frame = get_frame(cap)
-            cv2.imshow("MIRA", frame)
-            key = cv2.waitKey(1) & 0xFF
+            key = show(frame)
             cv2.imwrite("image.jpg", frame)
             return frame
         if key == ord("e"):
@@ -98,8 +112,14 @@ def photomaton_loop(cap, waiting_time=100):
             return
 
 
+cadre = cv2.imread('data/cadre.png')
+# cv2.imshow('cadre', cadre)
+# cv2.waitKey(0)
 
-cap = cv2.VideoCapture(1)
+# Close all windows
+# cv2.destroyAllWindows()
+h_cadre, w_cadre, _ = cadre.shape 
+cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
@@ -115,7 +135,7 @@ above_p1 = np.array([415, -63, 192])
 above_p2 = np.array([260, 63, 192])
 
 while True:
-    frame = photomaton_loop(cap, 0)
+    frame = photomaton_loop(cap, 70)
 
     edge_image = rgb2edge_image(frame)
 
