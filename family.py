88de782df49bd4bb5cae37ab273df25985275fdd
Kaduo <balthazarpotet@gmail.com>
2023-11-16 13:01:50 +0100

    change coords

--- family.py
+++ family.py
@@ -5,14 +5,15 @@ from arms import get_big_drawing_arm, calibrate, draw_edges
 import skimage as ski
 from pebble import concurrent
 import pickle
+from time import time
 
 from photo2drawing import rgb2edges, edge_image2edges
 
 arm = get_big_drawing_arm()
 
-above_origin = np.array([270, -107, 160])
-above_p1 = np.array([630, -107, 162])
-above_p2 = np.array([270, 158, 157])
+above_origin = np.array([220, -123, 142])
+above_p1 = np.array([527, -123, 146])
+above_p2 = np.array([220, 131, 142])
 
 @concurrent.process
 def make_converter(image):
@@ -31,15 +32,26 @@ def process_image(image, nb_edges=1000):
 
 if __name__=="__main__":
 
+    with open("data/jerome 2 edges.pkl", "rb") as f:
+        edges = pickle.load(f)
+        print("HEEREE")
+        print(len(edges))
+        f.close()
+    
+    # 9h25
+    start = time()
+
     image = ski.io.imread("data/st jerome.jpg")
     # future_edges = process_image(image)
     future_converter = make_converter(image)
 
     converter = future_converter.result()
 
-    with open("data/jerome 2 edges.pkl", "rb") as f:
-        edges = pickle.load(f)
-        f.close()
+
     
     edges = converter.convert_list_of_points(edges)
     draw_edges(arm, edges, verbose=True)
+
+    end = time()
+
+    print(end - start)
