4313dabb4904fbabc375dfb700a7af43fa2c73ee
Balthazar Potet <balthazarpotet@gmail.com>
2024-01-10 12:12:20 +0100

    Quick fix in photomaton.py

--- photomaton.py
+++ photomaton.py
@@ -117,55 +117,57 @@ def photomaton_loop(cap, waiting_time=100):
             return
 
 
-cadre = cv2.imread("data/cadre.png")
-h_cadre, w_cadre, _ = cadre.shape
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+if __name__ == "__main__":
+    cadre = cv2.imread("data/cadre.png")
+    h_cadre, w_cadre, _ = cadre.shape
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
 
+    with open("mira_correct.pkl", "rb") as f:
+        mira_data = pickle.load(f)
+        f.close()
 
-with open("mira_correct.pkl", "rb") as f:
-    mira_data = pickle.load(f)
-    f.close()
+    arm = get_photomaton_arm()
 
-arm = get_photomaton_arm()
+    above_origin = np.array([305, -60, 138])
+    above_p1 = np.array([435, -60, 138])
+    above_p2 = np.array([305, 60, 138])
 
-above_origin = np.array([305, -60, 138])
-above_p1 = np.array([435, -60, 138])
-above_p2 = np.array([305, 60, 138])
+    while True:
+        frame = photomaton_loop(cap, 10)
 
-while True:
-    frame = photomaton_loop(cap, 10)
+        edge_image = rgb2edge_image(frame, bilateral=False)
 
-    edge_image = rgb2edge_image(frame, bilateral=False)
+        edge_image[15 * edge_image.shape[0] // 16 :, 3 * edge_image.shape[1] // 4 :] = True
 
-    edge_image[15 * edge_image.shape[0] // 16 :, 3 * edge_image.shape[1] // 4 :] = True
+        edges = edge_image2edges(edge_image, nb_edges=700)
 
-    edges = edge_image2edges(edge_image, nb_edges=700)
+        drawing_in_progress(edge_image)
 
-    drawing_in_progress(edge_image)
+        arm.motion_enable(enable=True)
+        arm.set_mode(0)
+        arm.set_state(state=0)
 
-    arm.motion_enable(enable=True)
-    arm.set_mode(0)
-    arm.set_state(state=0)
+        origin, p1, p2 = calibrate(
+            arm, [above_origin, above_p1, above_p2], absolute_epsilon=[2, 5, 2]
+        )
 
-    origin, p1, p2 = calibrate(arm, [above_origin, above_p1, above_p2], absolute_epsilon=[2, 5, 2])
+        converter = CoordinatesConverter(list(reversed(edge_image.shape[:2])), origin, p1, p2)
+        sorted_edges = sort_edges(edges)
+        converted_edges = converter.convert_list_of_points(sorted_edges)
 
-    converter = CoordinatesConverter(list(reversed(edge_image.shape[:2])), origin, p1, p2)
-    sorted_edges = sort_edges(edges)
-    converted_edges = converter.convert_list_of_points(sorted_edges)
+        draw_edges(arm, converted_edges)
 
-    draw_edges(arm, converted_edges)
+        shift_mira = np.array([0, 10, 0])
+        origin_mira = origin + (3 / 4 * (p2 - origin) + 15 / 16 * (p1 - origin)) - shift_mira
+        p1_mira = origin_mira + 1 / 16 * (p1 - origin)
+        p2_mira = origin_mira + 1 / 4 * (p2 - origin)
+        mira_image = [np.max(mira_data[1]), np.max(mira_data[0])]
+        converterMIRA = CoordinatesConverter(mira_image, origin_mira, p1_mira, p2_mira)
 
-    shift_mira = np.array([0, 10, 0])
-    origin_mira = origin + (3 / 4 * (p2 - origin) + 15 / 16 * (p1 - origin)) - shift_mira
-    p1_mira = origin_mira + 1 / 16 * (p1 - origin)
-    p2_mira = origin_mira + 1 / 4 * (p2 - origin)
-    mira_image = [np.max(mira_data[1]), np.max(mira_data[0])]
-    converterMIRA = CoordinatesConverter(mira_image, origin_mira, p1_mira, p2_mira)
+        for letter in mira_data:
+            points = converterMIRA.convert(letter)
+            draw_edge(arm, points, speed=50, wait=True)
 
-    for letter in mira_data:
-        points = converterMIRA.convert(letter)
-        draw_edge(arm, points, speed=50, wait=True)
-
-    arm.set_position(x=0, y=0, z=30, speed=100, wait=True, relative=True)
+        arm.set_position(x=0, y=0, z=30, speed=100, wait=True, relative=True)
